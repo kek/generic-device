a6f1f85d6581f7babba74a127e3adbca024e6916
Karl Eklund <localpart@gmail.com>
2026-01-13 23:23:33 +0100

    Blink lights

--- micro.c
+++ micro.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_PIN GPIO_NUM_2
 
 void app_main(void)
 {
+    // Configure the LED pin as output
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
+    printf("Starting LED blink...\n");
+
+    while (1) {
+        // Turn LED on
+        gpio_set_level(LED_PIN, 1);
+        printf("LED ON\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        // Turn LED off
+        gpio_set_level(LED_PIN, 0);
+        printf("LED OFF\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
