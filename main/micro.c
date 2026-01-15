ca08a0258a5fee75edf4b5426b4a3d1bde15823f
Karl Eklund <localpart@gmail.com>
2026-01-16 00:44:14 +0100

    Remove unused code

--- main/micro.c
+++ main/micro.c
@@ -1,18 +1,8 @@
-#include "led.h"
-#include "system_info.h"
 #include "st7735.h"
 #include "image_data.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
 void app_main(void)
 {
-    // Initialize LED
-    led_init();
-
-    // Print system info
-    system_info_print();
-
     // Initialize ST7735 display
     st7735_init(INITR_BLACKTAB);
 
@@ -21,7 +11,4 @@ void app_main(void)
 
     // Display the image (160x128 pixels, fills entire screen in landscape mode)
     st7735_draw_image(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT, image_data);
-
-    // Blink LED forever (blocks)
-    led_blink_forever();
 }
